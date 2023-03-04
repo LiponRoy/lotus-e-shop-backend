899627ebf4216f14a091db926cffa90223240605
Lipon Roy <liponbdbd@gmail.com>
2023-03-04 14:47:46 +0600

    some changes

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { createProduct, getAllProduct, getProduct } from '../controllers/product.js';
-// import { verifyToken, verifyUser, verifyAdmin } from '../utils/VerifyTokenJwt.js';
+import { verifyToken, verifyUser, verifyAdmin } from '../utils/VerifyTokenJwt.js';
 const productRouter = express.Router();
 
 productRouter.post('/create', createProduct);
 productRouter.get('/getAll', getAllProduct);
-productRouter.get('/getOne/:id', getProduct);
+productRouter.get('/getOne/:id', verifyToken, getProduct);
 
 export default productRouter;
